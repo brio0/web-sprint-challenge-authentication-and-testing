2709c37107a1d1c26986e312046ea8559c154489
Bryan Andres <bry.andres001@gmail.com>
2023-06-14 19:31:11 -1000

    wip register

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -1,7 +1,7 @@
 const db = require('../../data/dbConfig')
 
 function find() {
-    return db('users').select('id', 'username',)
+    return db('users').select('id', 'username')
 }
 function findById(id) {
     return db('users').where('id', id).first()
@@ -22,5 +22,4 @@ module.exports = {
     add,
     findById,
     findByUsername,
-    findBy
 }
